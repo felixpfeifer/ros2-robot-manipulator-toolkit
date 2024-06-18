341163c7677ab92d82bf897b8cbb429910ba394d
Felix Pfeifer <fpfeifer@stud.hs-heilbronn.de>
2024-06-18 16:50:51 +0200

    Added the Terminal Controller

--- robo_teleoperation/src/teleoperation_backend_node.cpp
+++ robo_teleoperation/src/teleoperation_backend_node.cpp
@@ -3,7 +3,7 @@
  * Description: Backend node for the teleoperation interface
  * Author:      Felix Pfeifer
  * Email:       fpfeifer@stud.hs-heilbronn.de
- * Version:     2.0
+ * Version:     2.1
  * License:     MIT License
  ******************************************************************************/
 
@@ -23,9 +23,38 @@ namespace robo_teleoperation {
 
         moveGroupInterface = std::make_shared<moveit::planning_interface::MoveGroupInterface>(node, PLANNING_GROUP);
 
-        // Create a Subscriber for the Teleoperation Interfaces and call the Callback Function moveRobot
-        teleoperation_interface_subscription = this->create_subscription<robot_teleoperation_interface::msg::Teleop>(
-                "teleop_command_jog", 10, std::bind(&TeleoperationBackendNode::moveRobot, this, std::placeholders::_1));
+        // Initialize the Services as Server
+        move_robot_service = this->create_service<robot_teleoperation_interface::srv::MoveRobot>(
+                "move_robot", std::bind(&TeleoperationBackendNode::moveRobotService, this, std::placeholders::_1,
+                                        std::placeholders::_2));
+
+        move_point_service = this->create_service<robot_teleoperation_interface::srv::MovePoint>(
+                "move_point", std::bind(&TeleoperationBackendNode::movePointService, this, std::placeholders::_1,
+                                        std::placeholders::_2));
+
+        select_tool_service = this->create_service<robot_teleoperation_interface::srv::SelectTool>(
+                "select_tool", std::bind(&TeleoperationBackendNode::selectToolService, this, std::placeholders::_1,
+                                         std::placeholders::_2));
+        allign_tcp_service = this->create_service<robot_teleoperation_interface::srv::AllignTCP>(
+                "allign_tcp", std::bind(&TeleoperationBackendNode::allignTCPService, this, std::placeholders::_1,
+                                        std::placeholders::_2));
+        hand2_eye_service = this->create_service<robot_teleoperation_interface::srv::Hand2Eye>(
+                "hand2_eye", std::bind(&TeleoperationBackendNode::hand2EyeService, this, std::placeholders::_1,
+                                       std::placeholders::_2));
+
+        teach_point_service = this->create_service<robot_teleoperation_interface::srv::TeachPoint>(
+                "teach_point", std::bind(&TeleoperationBackendNode::teachPointService, this, std::placeholders::_1,
+                                         std::placeholders::_2));
+
+        tool_service = this->create_service<robot_teleoperation_interface::srv::Tool>(
+                "tool", std::bind(&TeleoperationBackendNode::toolService, this, std::placeholders::_1,
+                                  std::placeholders::_2));
+
+        // GPIO Publisher and Subscriber
+        gpio_publisher = this->create_publisher<CmdType>("/gpio_controller/commands", 10);
+        gpio_subscriber = this->create_subscription<CmdType>(
+                "/gpio_controller/inputs", 10,
+                std::bind(&TeleoperationBackendNode::gpioCallback, this, std::placeholders::_1));
 
         // Use to move the Robot the OMPL Planner with the RRTConnect Algorithm
         moveGroupInterface->setPlannerId("RRTConnect");
@@ -45,7 +74,7 @@ namespace robo_teleoperation {
         RCLCPP_INFO(this->get_logger(), "Teleoperation Backend Node is ready to receive commands");
 
         // Timer that runs every 20ms to get the current state of the robot
-        timer_ = this->create_wall_timer(1s, std::bind(&TeleoperationBackendNode::timer_callback, this));
+        //timer_ = this->create_wall_timer(1s, std::bind(&TeleoperationBackendNode::timer_callback, this));
 
     }
 
@@ -318,10 +347,17 @@ namespace robo_teleoperation {
         pitch = round(pitch / 90) * 90;
         yaw = round(yaw / 90) * 90;
 
+        RCLCPP_INFO(this->get_logger(), "New Orientation: roll: %f, pitch: %f, yaw: %f", roll, pitch, yaw);
+
         // Convert the orientation angels from degree to rad
         roll = roll * M_PI / 180;
         pitch = pitch * M_PI / 180;
         yaw = yaw * M_PI / 180;
+
+        // Set the new orientation
+        q.setRPY(roll, pitch, yaw);
+        tf2::convert(q, current_pose.pose.orientation);
+
 
         // Move the robot to the new orientation
         moveWorldPose(current_pose.pose);
@@ -342,6 +378,238 @@ namespace robo_teleoperation {
         q.setRPY(roll_, pitch_, yaw_);
         tf2::convert(q, target_pose.orientation);
         return target_pose;
+    }
+
+    void TeleoperationBackendNode::safePosetoMongoDB(geometry_msgs::msg::Pose pose, std::string name) {
+
+        // Safe the Pose to the MongoDB
+        RCLCPP_INFO(this->get_logger(), "Safe Pose to MongoDB");
+        // Use the Poses Collection
+        // Create a new Document
+        auto doc = bsoncxx::builder::stream::document{};
+        // Add the Name of the Pose to the Document
+        doc << "name" << name;
+        doc << "tcp_link" << moveGroupInterface->getEndEffectorLink();
+        // Add the Position of the Pose to the Document
+        doc << "position" << bsoncxx::builder::stream::open_array
+            << pose.position.x
+            << pose.position.y
+            << pose.position.z
+            << bsoncxx::builder::stream::close_array;
+        // Add the Orientation of the Pose to the Document
+        doc << "orientation" << bsoncxx::builder::stream::open_array
+            << pose.orientation.x
+            << pose.orientation.y
+            << pose.orientation.z
+            << pose.orientation.w
+            << bsoncxx::builder::stream::close_array;
+
+        // Insert the Document to the Collection
+        auto result = poses.insert_one(doc.view());
+        RCLCPP_INFO(this->get_logger(), "Inserted Pose with Name: %s", name.c_str());
+
+    }
+
+    void TeleoperationBackendNode::moveRobotService(
+            const std::shared_ptr<robot_teleoperation_interface::srv::MoveRobot::Request> request,
+            std::shared_ptr<robot_teleoperation_interface::srv::MoveRobot::Response> response) {
+        RCLCPP_INFO(this->get_logger(), "Move Robot Service");
+        // Get the Frame in which the Robot should move
+        int frame = request->frame_id;
+        if (frame == 0) { // Move Joint
+            std::vector<double> joint_values(6, 0.0);
+            // Set the vector to the old values
+            joint_values = moveGroupInterface->getCurrentJointValues();
+            // Find in the tokes first the axis and then the value
+            for (int i = 0; i < request->data.size(); i++) {
+                int axis = request->axis.at(i);
+                double value = request->data.at(i);
+                joint_values[axis] = value;
+            }
+            // Print out the Joint Values
+            for (int i = 0; i < joint_values.size(); ++i) {
+                RCLCPP_INFO(this->get_logger(), "Joint %d: %f", i, joint_values[i]);
+            }
+            moveJoint(joint_values, false);
+
+        } else if (frame == 1) { // Move World
+            geometry_msgs::msg::Pose pose = moveGroupInterface->getCurrentPose().pose;
+            // Add the values of the array to the current Pose
+            // Print Info about Reqeust
+            RCLCPP_INFO(this->get_logger(), "Size of data Vector is %d", request->data.size());
+            RCLCPP_INFO(this->get_logger(), "Size of axis Vector is %d", request->axis.size());
+
+            // Print the Current Pose
+            RCLCPP_INFO(this->get_logger(), "Current Pose: x: %f, y: %f, z: %f, qx: %f, qy: %f, qz: %f, qw: %f",
+                        pose.position.x, pose.position.y, pose.position.z, pose.orientation.x, pose.orientation.y,
+                        pose.orientation.z, pose.orientation.w);
+
+            for (int i = 0; i < request->data.size(); ++i) {
+                int id = request->axis.at(i);
+                switch (id) {
+                    case 0:
+                        pose.position.x += request->data.at(i);
+                        break;
+                    case 1:
+                        pose.position.y += request->data.at(i);
+                        break;
+                    case 2:
+                        pose.position.z += request->data.at(i);
+                        break;
+                    case 3:
+                        changeOrientation(pose, request->data.at(i), 0);
+                        break;
+                    case 4:
+                        changeOrientation(pose, request->data.at(i), 1);
+                        break;
+                    case 5:
+                        changeOrientation(pose, request->data.at(i), 2);
+                        break;
+                    default:
+                        RCLCPP_ERROR(this->get_logger(), "Invalid Axis ID");
+                }
+            }
+            // Print the Current Pose
+            RCLCPP_INFO(this->get_logger(), "New Pose: x: %f, y: %f, z: %f, qx: %f, qy: %f, qz: %f, qw: %f",
+                        pose.position.x, pose.position.y, pose.position.z, pose.orientation.x, pose.orientation.y,
+                        pose.orientation.z, pose.orientation.w);
+            moveWorldPose(pose);
+        }
+    }
+
+    void TeleoperationBackendNode::selectToolService(
+            const std::shared_ptr<robot_teleoperation_interface::srv::SelectTool::Request> request,
+            std::shared_ptr<robot_teleoperation_interface::srv::SelectTool::Response> response) {
+        RCLCPP_INFO(this->get_logger(), "Select Tool Service");
+        // Select the Tool
+        switch (request->tcp_id) {
+            case 0:
+                // Flansch of the Robot Link_6_1
+                moveGroupInterface->setEndEffectorLink("Link_6_1");
+                response->success = true;
+                response->message = "Selected Tool: Flansch";
+                break;
+            case 1:
+                // Camera
+                switchEndEffector(false);
+                response->success = true;
+                response->message = "Selected Tool: Camera";
+                break;
+            case 2:
+                // Gripper
+                switchEndEffector(true);
+                response->success = true;
+                response->message = "Selected Tool: Gripper";
+                break;
+            default:
+                RCLCPP_ERROR(this->get_logger(), "Invalid Tool ID");
+                response->success = false;
+                response->message = "Invalid Tool ID";
+        }
+
+    }
+
+    void TeleoperationBackendNode::allignTCPService(
+            const std::shared_ptr<robot_teleoperation_interface::srv::AllignTCP::Request> request,
+            std::shared_ptr<robot_teleoperation_interface::srv::AllignTCP::Response> response) {
+        RCLCPP_INFO(this->get_logger(), "Allign TCP Service");
+        // Align the TCP to the World Orientation
+        // Check if pose is emtpy
+        if (request->hs_pose == false) {
+            RCLCPP_INFO(this->get_logger(), "Align %s to World Orientation",
+                        moveGroupInterface->getEndEffectorLink().c_str());
+            alignTCP();
+            response->success = true;
+            return;
+        }
+        response->success = false;
+
+    }
+
+    void TeleoperationBackendNode::hand2EyeService(
+            const std::shared_ptr<robot_teleoperation_interface::srv::Hand2Eye::Request> request,
+            std::shared_ptr<robot_teleoperation_interface::srv::Hand2Eye::Response> response) {
+
+    }
+
+    void TeleoperationBackendNode::teachPointService(
+            const std::shared_ptr<robot_teleoperation_interface::srv::TeachPoint::Request> request,
+            std::shared_ptr<robot_teleoperation_interface::srv::TeachPoint::Response> response) {
+        RCLCPP_INFO(this->get_logger(), "Teach Point Service");
+        auto pose = moveGroupInterface->getCurrentPose().pose;
+        safePosetoMongoDB(pose, request->name);
+        response->success = true;
+
+
+    }
+
+    void TeleoperationBackendNode::toolService(
+            const std::shared_ptr<robot_teleoperation_interface::srv::Tool::Request> request,
+            std::shared_ptr<robot_teleoperation_interface::srv::Tool::Response> response) {
+        RCLCPP_INFO(this->get_logger(), "Tool Service");
+        CmdType cmd;
+        if (request->close) {
+            cmd.data.push_back(1.0);
+        } else {
+            // Open the Gripper by setting the first GPIO to Zero
+            cmd.data.push_back(0.0);
+
+        }
+        gpio_publisher->publish(cmd);
+        response->success = true;
+    }
+
+    void TeleoperationBackendNode::gpioCallback(const CmdType::SharedPtr msg) {
+        RCLCPP_INFO(this->get_logger(), "GPIO Callback");
+        // Check if the GPIO is not empty
+        if (msg->data.size() > 0) {
+            // Check if the GPIO is the Homing GPIO
+            if (msg->data[0] > 1.0) {
+                gripper_open = true;
+            } else {
+                gripper_open = false;
+            }
+        }
+
+    }
+
+    void TeleoperationBackendNode::movePointService(
+            const std::shared_ptr<robot_teleoperation_interface::srv::MovePoint::Request> request,
+            std::shared_ptr<robot_teleoperation_interface::srv::MovePoint::Response> response) {
+        RCLCPP_INFO(this->get_logger(), "Move Point Service");
+        RCLCPP_INFO(this->get_logger(), "Move Robot to Point: %s", request->point_name.c_str());
+        // Get the Pose from the MongoDB
+        auto filter = bsoncxx::builder::stream::document{} << "name" << request->point_name
+                                                           << bsoncxx::builder::stream::finalize;
+        auto result = poses.find_one(filter.view());
+        if (result) {
+            // Get the Pose from the Document
+            auto view = result->view();
+
+            // Get the Position of the Pose
+            auto position = view["position"].get_array().value;
+            geometry_msgs::msg::Point point;
+            point.x = position[0].get_double();
+            point.y = position[1].get_double();
+            point.z = position[2].get_double();
+            // Get the Orientation of the Pose
+            auto orientation = view["orientation"].get_array().value;
+            geometry_msgs::msg::Quaternion quaternion;
+            quaternion.x = orientation[0].get_double();
+            quaternion.y = orientation[1].get_double();
+            quaternion.z = orientation[2].get_double();
+            quaternion.w = orientation[3].get_double();
+            // Create the Pose
+            geometry_msgs::msg::Pose pose;
+            pose.position = point;
+            pose.orientation = quaternion;
+            // Move the Robot to the Pose
+            moveWorldPose(pose);
+            response->success = true;
+        } else {
+            RCLCPP_ERROR(this->get_logger(), "Pose not found");
+            response->success = false;
+        }
     }
 
 }
